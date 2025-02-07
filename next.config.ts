9b29f7cf9e421bab8d7731ba47e3e5d018028db2
Rafal Piekara <rafpiek@gmail.com>
2025-02-07 14:39:25 +0100

    setup for deep links

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,27 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async headers() {
+    return [
+      {
+        source: '/dl/:path*',
+        headers: [
+          {
+            key: 'X-Apple-App-Clip-Bundle-ID',
+            value: 'net.dinecloud.customers.Clip'
+          },
+          {
+            key: 'Content-Type',
+            value: 'text/html; charset=utf-8'
+          },
+          {
+            key: 'X-Apple-App-Clip-Bundle-ID',
+            value: 'net.dinecloud.customers.Clip'
+          }
+        ],
+      },
+    ]
+  },
 };
 
 export default nextConfig;
